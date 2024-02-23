b4b0037398419bf0a9094d897f5134d2f19221fb
lingowmx <clingow@hotmail.com>
2024-02-23 12:24:29 -0500

    all good before localstorage

--- app.js
+++ app.js
@@ -1,1 +1,80 @@
-console.log("ok")
+const form = document.querySelector("#form");
+const tasksList = document.querySelector("#tasks-list");
+const template = document.querySelector("#template").content;
+const fragment = document.createDocumentFragment();
+let input = document.querySelector("input");
+let tasks = {};
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("drawtask call")
+    drawTasks()
+})
+
+form.addEventListener("submit", (event) => {
+  console.log(event.target.querySelector("input").value);
+  let inputValue = event.target.querySelector("input").value;
+  event.preventDefault(event);
+  setTasks(inputValue);
+});
+
+tasksList.addEventListener("click", (event) => {
+  btnAction(event);
+});
+
+const setTasks = (inputValue) => {
+  if (inputValue.trim() === "") {
+    console.log("empty");
+    return;
+  }
+  const tarea = {
+    id: Date.now(),
+    title: inputValue,
+    state: false,
+  };
+
+  tasks[tarea.id] = tarea;
+  form.reset();
+  input.focus();
+
+  drawTasks();
+};
+
+const drawTasks = () => {
+  tasksList.innerHTML = " ";
+  Object.values(tasks).forEach((task) => {
+    console.log(task);
+    const clone = template.cloneNode(true);
+    clone.querySelector("p").textContent = task.title;
+    if (task.state) {
+      clone
+        .querySelector(".alert")
+        .classList.replace("alert-warning", "alert-primary");
+      clone
+        .querySelectorAll(".fas")[0]
+        .classList.replace("fa-check-circle", "fa-rotate-left");
+      clone.querySelector("p").style.textDecoration = "line-through";
+    }
+    clone.querySelectorAll(".fas")[0].dataset.id = task.id;
+    clone.querySelectorAll(".fas")[1].dataset.id = task.id;
+    fragment.appendChild(clone);
+  });
+  tasksList.appendChild(fragment);
+};
+
+const btnAction = (event) => {
+  console.log(event.target.classList.contains("fa-check-circle"));
+  if (event.target.classList.contains("fa-check-circle")) {
+    tasks[event.target.dataset.id].state = true;
+    console.log(tasks);
+    drawTasks();
+  }
+  if (event.target.classList.contains("fa-minus-circle")) {
+    delete tasks[event.target.dataset.id];
+    drawTasks();
+  }
+  if (event.target.classList.contains("fa-rotate-left")) {
+    tasks[event.target.dataset.id].state = false;
+    drawTasks();
+  }
+  event.stopPropagation();
+};
